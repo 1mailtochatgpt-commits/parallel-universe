05e8db1f67586ae043a3d5a06ad0c759c6dbbfa9
1mailtochatgpt-commits <1mailtochatgpt@gmail.com>
2026-02-14 17:09:13 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE = "our-universe-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./assets/us.jpg",
+  "./assets/ambient.mp3",
+  "./assets/rain.mp3"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
